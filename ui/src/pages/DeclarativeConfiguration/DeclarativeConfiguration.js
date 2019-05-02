2185ef4c575df7c2017d7c1837157de87dc2cbe4
Richard Li <richard@datawire.io>
2019-05-02 14:01:36 -0400

    add more text

--- ui/src/pages/DeclarativeConfiguration/DeclarativeConfiguration.js
+++ ui/src/pages/DeclarativeConfiguration/DeclarativeConfiguration.js
@@ -5,6 +5,39 @@ import './styles.scss';
 const DeclarativeConfiguration = () =>
 <div>
   <h1>Declarative Configuration</h1>
+
+  <p>Ambassador is configured <strong>declaratively</strong>. Unlike traditional
+  API Gateways that are configured using REST APIs or UIs, you configure
+  Ambassador through Kubernetes-style annotations. For example, here is
+  the configuration for the `tour` service that you're using right now:
+  </p>
+
+  <pre>
+  ---
+apiVersion: v1
+kind: Service
+metadata:
+  name: tour
+  annotations:
+    getambassador.io/config: |
+      ---
+      apiVersion: ambassador/v1
+      kind:  Mapping
+      name:  tour_mapping
+      prefix: /tour/
+      service: tour
+spec:
+  ports:
+  - name: http
+    port: 80
+  </pre>
+
+  <p>This is a standard Kubernetes service called `tour`. In the service, we create
+  an Ambassador `mapping` resource which maps requests from `/` to `service: tour`.
+  Ambassador exposes a rich set of configuration options for the mapping object,
+  including timeouts, load balancing options, traffic weights, and more. For details,
+  see <a href="https://www.getambassador.io/reference/mappings">the mapping documentation</a>.</p>
+  
 </div>;
 
 export default DeclarativeConfiguration;
--- ui/src/pages/Welcome/Welcome.js
+++ ui/src/pages/Welcome/Welcome.js
@@ -4,7 +4,12 @@ import './styles.scss';
 
 const Welcome = () =>
 <div>
-  <h1>Welcome to the ambassador tour</h1>
+  <h1>Congratulations!</h1>
+  <h2>You've successfully installed the Ambassador API Gateway.</h2>
+
+  <p>Ambassador is an open source, Kubernetes-native API Gateway built on <a href="https://www.envoyproxy.io">Envoy Proxy</a>.
+  In this tour, we'll walk you through some of the key features of Ambassador.</p>
+
 </div>;
 
 export default Welcome;
