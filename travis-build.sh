2bd61dadb0a4cd7262f8a67f798855c5687fc40f
nbkrause <krausenoah@gmail.com>
2019-05-10 16:16:55 -0400

    renamed quote -> backend
    Signed-off-by: nbkrause <krausenoah@gmail.com>

--- travis-build.sh
+++ travis-build.sh
@@ -3,6 +3,6 @@ DOCKER_REPO="quay.io/datawire/tour"
 
 docker login -u="datawire+releasebot" -p=$DOCKER_PASSWORD quay.io
 
-( cd quote && make image.push DOCKER_REPO=$DOCKER_REPO TAG=$TRAVIS_TAG )
+( cd backend && make image.push DOCKER_REPO=$DOCKER_REPO TAG=$TRAVIS_TAG )
 
 ( cd ui && make image.push DOCKER_REPO=$DOCKER_REPO TAG=$TRAVIS_TAG )
