2ec0b6618f97c8ebc258b0d81719d31bc6305465
nbkrause <krausenoah@gmail.com>
2019-05-13 12:44:10 -0400

    Add sso instructions
    Signed-off-by: nbkrause <krausenoah@gmail.com>

--- ui/src/pages/GitHub/GitHub.js
+++ ui/src/pages/GitHub/GitHub.js
@@ -21,21 +21,36 @@ const GitHub = () =>
         </ul>
       </ul>
     </li>
-    <li>Edit your <code>env.sh</code> and add the <code>CLIENT_ID</code> and <code>CLIENT_SECRET</code> from GitHub:
+    <li>In the <code>sso/</code> directory, edit the <code>github.yaml</code> <code>ConfigMap</code> declaration and add the <code>{`{{CLIENT_ID}}`}</code> and <code>{`{{CLIENT_SECRET}}`}</code> from GitHub:
 
       <div className="code-block">
         <pre>
           <code>
-{`CLIENT_ID=Client ID from GitHub
-CLIENT_SECRET=Client Secret from GitHub`}
+{`---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: github-app
+data:
+  clientId: {{CLIENT_ID}}
+  clientSecret: {{CLIENT_SECRET}}`}
           </code>
         </pre>
       </div>
      </li>
      <li>Get the <code>External-IP</code> for your Ambassador service <code>kubectl get svc ambassador</code>.</li>
-     <li>Replace the <code>$AMBASSADOR_IP</code> values in [api-auth-with-github/00-tenant.yaml](00-tenant.yaml)</li>
-     <li>Run <code>make apply-api-auth</code>.</li>
-    <li>Go to <code>https://$AMBASSADOR_IP/httpbin/headers</code> in your browser and you will be asked to login. Select the <code>GitHub</code> option.</li>
+     <li>Replace the <code>$AMBASSADOR_IP</code> and <code>$PROTO</code> values in <code>sso/00-tenant.yaml</code></li>
+     <li>Deploy Keycloak, the <code>Filter</code> and <code>FilterPolicy</code> with <code>kubectl</code>.
+     
+       <div className="code-block">
+         <pre>
+           <code>
+{`kubectl apply -f sso/`}             
+           </code>
+         </pre>
+       </div>
+     </li>
+    <li>Refresh the page and you will be required to login. Select the <code>GitHub</code> option to login with your GitHub credentials.</li>
   </ol>
 
 </div>;
