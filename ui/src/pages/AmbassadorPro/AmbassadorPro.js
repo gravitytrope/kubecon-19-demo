e5fec7ca425d27e277482abe8fd55a5a7054bef7
Richard Li <richard@datawire.io>
2019-05-17 08:40:15 -0400

    edit text

--- ui/src/pages/AmbassadorPro/AmbassadorPro.js
+++ ui/src/pages/AmbassadorPro/AmbassadorPro.js
@@ -10,7 +10,8 @@ const AmbassadorPro = () =>
   <p>Ambassador Pro is a commercial version of Ambassador. Ambassador includes additional
   capabilities around security (e.g., authentication, rate limiting) and developing
   microservices (e.g., Telepresence integration). If you're interested in trying
-  Ambassador Pro, sign up for a free, 14 day trial below:</p>
+  Ambassador Pro, sign up for a free, 14 day trial below. You'll receive an email
+  in a few minutes with a license key that you can use.</p>
 
   <section className="free-trial-form">
     <HubspotForm
