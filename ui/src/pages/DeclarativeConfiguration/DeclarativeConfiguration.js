75d6edeab46eb9cc7c602f368d6be2dd93801f71
nbkrause <krausenoah@gmail.com>
2019-05-22 14:22:46 -0400

    revert back to using annotations
    Signed-off-by: nbkrause <krausenoah@gmail.com>

--- ui/src/pages/DeclarativeConfiguration/DeclarativeConfiguration.js
+++ ui/src/pages/DeclarativeConfiguration/DeclarativeConfiguration.js
@@ -8,7 +8,7 @@ const DeclarativeConfiguration = () =>
 
   <p>Ambassador is configured <strong>declaratively</strong>. Unlike traditional
   API Gateways that are configured using REST APIs or UIs, you configure
-  Ambassador through Kubernetes Custom Resources. Not only is this approach
+  Ambassador through Kubernetes service annotations or Custom Resources. Not only is this approach
   consistent with Kubernetes, but it also makes it possible (and recommended!) for you
   to manage all your configuration under source control.</p>
   
@@ -21,25 +21,18 @@ const DeclarativeConfiguration = () =>
       <pre>
         <code>
     {`---
-apiVersion: getambassador.io/v1
-kind: Mapping
-metadata:
-  name: tour-ui
-spec:
-  prefix: /
-  service: tour:5000
-`}
-        </code>
-    </pre>
-  </div>
-  <div className="code-block">
-      <pre>
-        <code>
-    {`---
 apiVersion: v1
 kind: Service
 metadata:
   name: tour
+  annotations:
+    getambassador.io/config: |
+      ---
+      apiVersion: ambassador/v1
+      kind: Mapping
+      name: tour-ui_mapping
+      prefix: /
+      service: tour:5000
 spec:
   ports:
   - name: ui
@@ -59,9 +52,25 @@ spec:
     Above is a standard Kubernetes service called <code>tour</code> that exposes 
     port <code>5000</code> for this React application and <code>8080</code> for 
     the golang backend running in the <code>tour</code> pod.
-    Using a <code>Mapping</code> resource, we tell Ambassador to map requests
+    Using a <code>Mapping</code> annotation, we tell Ambassador to map requests
     from <code>/</code> to <code>service: tour:5000</code> so requests
     to <code>http://{`{AMBASSADOR_IP}`}/</code> are routed to this React application.
+    An example of this using a <code>Mapping</code> custom resource is shown below:
+    <div className="code-block">
+      <pre>
+        <code>
+    {`---
+apiVersion: getambassador.io/v1
+kind: Mapping
+metadata:
+  name: tour-ui
+spec:
+  prefix: /
+  service: tour:5000
+`}
+        </code>
+    </pre>
+  </div>
     Ambassador exposes a rich set of configuration options for the mapping object,
     including timeouts, load balancing options, traffic weights, and more. For details,
     see <a href="https://www.getambassador.io/reference/mappings">the mapping documentation</a>.
