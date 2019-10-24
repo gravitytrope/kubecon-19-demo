abaf02cd0be4ab1d78038e91ab982029fb301f04
Alvaro Saurin <alvaro.saurin@gmail.com>
2019-10-24 20:54:11 +0200

    New ManifestArea component
    Signed-off-by: Alvaro Saurin <alvaro.saurin@gmail.com>

--- ui/src/pages/RateLimitingAction/RateLimitingAction.js
+++ ui/src/pages/RateLimitingAction/RateLimitingAction.js
@@ -1,29 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 import './styles.scss';
 
 import RealTimeGraph from '../../components/RealTimeGraph';
 import aeslogo from "../../images/aes-logo.png";
 import styles from "../../components/RealTimeGraph/styles.module.scss";
+import ManifestArea from '../../components/ManifestArea';
+import SliderTemplate from '../../components/SliderTemplate';
 
-const RateLimitingAction = () =>
-        <div>
 
-            <p>
-                <img className={styles.center} src={aeslogo} alt="Ambassador Edge Stack"/>
-            </p>
-
-            <center>
-              <h1>Rate Limiting in Action</h1>
-            </center>
-            <section className={styles.container}>
-
-                <div className={styles.half}><RealTimeGraph/></div>
-
-                <div className={[styles.half, "code-block"].join(' ')}>
-                  <pre>
-                    <code>
-{`apiVersion: getambassador.io/v1beta1
+const template=`---
+apiVersion: getamabassador.io/v1beta1
 kind: RateLimit
 metadata:
   name: backend-rate-limit
@@ -31,19 +22,75 @@ spec:
   domain: ambassador
   limits:
   - pattern: [{generic_key: "backend"}]
-    rate: 50
+    rate: [VALUE]
     unit: second
-`}
-                    </code>
-                  </pre>
-                  <br/>
-                    <button className={styles.Button}>
-                        kubectl apply -f ratelimit.yaml
-                    </button>
+`
 
-                </div>
-            </section>
-        </div>
-;
+///////////////////////////////////////////////////////////////////////////////////////
+
+class RateLimitingAction extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      manifest: ""
+    };
+
+    this.handleManifestChange = this.handleManifestChange.bind(this);
+  }
+
+  handleManifestChange(value) {
+    this.setState({
+      manifest: value
+    });
+  }
+
+  render() {
+    const useStyles = makeStyles(theme => ({
+      root: {
+        flexGrow: 1,
+      },
+      paper: {
+        padding: 15,
+        textAlign: 'center',
+      },
+    }));
+
+    return (
+        <div>
+          <p>
+              <img className={styles.center} src={aeslogo} alt="Ambassador Edge Stack"/>
+          </p>
+
+          <div className={useStyles.root}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Typography variant="h3">
+                  <center>
+                    Rate Limiting in Action
+                  </center>
+                </Typography>
+              </Grid>
+
+              <Grid item xs={6}>
+                  <RealTimeGraph/>
+              </Grid>
+
+              <Grid item xs={6}>
+                <Grid container direction="row" justify="flex-start" alignItems="flex-end">
+                  <Grid item xs={8}>
+                    <ManifestArea manifest={this.state.manifest} />
+                  </Grid>
+                  <Grid item xs={3}></Grid>
+                    <SliderTemplate template={template} default="100" onChange={this.handleManifestChange} />
+                  </Grid>
+                </Grid>
+              </Grid>
+
+
+          </div>
+      </div>
+    );
+  }
+}
 
 export default RateLimitingAction;
